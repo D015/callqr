5cf8babc149761d38540a0931566f425cbff72c0
Dmitry Zagorevskiy <7281015@gmail.com>
2020-11-11 23:31:45 +0100

    + class AdminAccess

---
+++ db_access/admin_access.py
@@ -0,0 +1,37 @@
+from models import Admin, User
+from app import db
+
+
+def create_creator_admin(creator_user_id, about, email, phone, user_id,
+                         corporation_id):
+    admin = Admin(creator_user_id=creator_user_id, active=True, about=about,
+                  email=email, phone=phone, role_admin_id=1, user_id=user_id,
+                  corporation_id=corporation_id)
+    db.session.add(admin)
+    return admin
+
+
+def create_admin(creator_user_id, about, email, phone, role_admin_id,
+                 corporation_id):
+    admin = Admin(creator_user_id=creator_user_id, about=about, email=email,
+                  phone=phone, role_admin_id=role_admin_id,
+                  corporation_id=corporation_id)
+    db.session.add(admin)
+    db.session.commit()
+    return admin
+
+
+def create_relationship_admin_to_user(admin_slug, user):
+    admin = Admin.query.filter(
+        Admin.slug == admin_slug, Admin.archived is False).first()
+    user_admin_corporation = user.admins.filter_by(
+        corporation_id=admin.corporation_id).first()
+    if admin.user_id or user_admin_corporation \
+            or user.archived or user.active is False:
+        pass
+    else:
+        admin.id = user.id
+        admin.active = True
+        db.session.add(admin)
+        db.session.commit()
+        return admin
--- db_access/corporation_access.py
+++ db_access/corporation_access.py
@@ -1,18 +1,18 @@
-from app import db
-
 from models import Corporation
 
 from access import create_creator_admin
 
 
-def create_corporation(name_corporation, about_corporation,
-                       about_admin, email_admin, phone_admin):
-    corporation = Corporation(name=name_corporation, about=about_corporation)
+def create_corporation(creator_user_id, name_corporation, about_corporation,
+                       about_admin, email_admin, phone_admin, user_id):
+    corporation = Corporation(creator_user_id=creator_user_id,
+                              name=name_corporation, about=about_corporation)
     try:
         db.session.add(corporation)
         db.session.flush()
-        admin = create_creator_admin(about=about_admin, email=email_admin,
-                                     phone=phone_admin,
+        admin = create_creator_admin(creator_user_id=creator_user_id,
+                                     about=about_admin, email=email_admin,
+                                     phone=phone_admin, user_id=user_id,
                                      corporation_id=corporation.id)
         db.session.commit()
         return corporation, admin
